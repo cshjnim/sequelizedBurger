8e0da8a7d986562289c3fae755e5cb8ab4855504
Hyejin Kim <cshjkim@gmail.com>
2020-01-30 22:19:27 -0800

    first commit

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+// set up the code to connect node to MySQL
+
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "986680Hj!",
+  database: "burgers_db"
+  });
+};
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+//export the connection
+
+module.exports = connection;
